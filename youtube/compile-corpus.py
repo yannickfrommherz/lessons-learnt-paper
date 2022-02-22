869cace3bcc4459190aa2db34862c43d888f2a0d
jaddla <jan.langenhorst@tu-dresden.de>
2022-02-22 16:05:37 +0100

    Added compile-corpus, removed script for tokenizing single transcript

---
+++ youtube/compile-corpus.py
@@ -0,0 +1,19 @@
+import nltk
+import json
+
+import os
+
+RESULTS_DIR = "results/"
+
+for file in os.listdir("results/"):
+	if file.endswith(".json"):
+
+		f = open(RESULTS_DIR + file)
+		transcript_data = json.load(f)
+		transcript_text = " ".join(transcript_data["transcript"])
+
+		tokens = nltk.word_tokenize(transcript_text, language='german')
+
+
+		for token in tokens:
+			print(token)
