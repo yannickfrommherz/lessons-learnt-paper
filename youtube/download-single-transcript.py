1fcf8b9aa76220b59f98335b64a7ec71e8945222
jaddla <jan.langenhorst@tu-dresden.de>
2022-02-22 11:11:59 +0100

    changed youtube example structure

--- youtube/download-single-transcript.py
+++ youtube/download-single-transcript.py
@@ -3,6 +3,11 @@ import sys
 import json
 from requests_html import HTMLSession
 
+RESULTS_DIR = "results/"
+
+if len(sys.argv) != 2:
+	exit(1)
+
 id = sys.argv[1]
 
 
@@ -14,13 +19,10 @@ transcript = YouTubeTranscriptApi.get_transcript(id, languages=['de'])
 text = ""
 lines = []
 
-word_count = 0
 
 for line in transcript:
-	word_count += len(line['text'].split())
 	lines.append(line['text'])
 
-print("word_count = " + str(word_count))
 
 result['title'] = title.replace(" - YouTube", "")
 result['video_id'] = id
@@ -28,5 +30,7 @@ result['transcript'] = lines
 # result['transcript_was_automatically_generated'] = transcript.is_generated
 
 
-json_string = json.dumps(result, ensure_ascii=False).encode('utf8')
-print(json_string.decode())
+json_string = json.dumps(result, ensure_ascii=False, indent=4).encode('utf8')
+open(RESULTS_DIR + id + ".json", "w").write(json_string.decode())
+
+
