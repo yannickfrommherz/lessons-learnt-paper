67eb95634201ef5a749a660a824bd9381c86ece5
jaddla <jan.langenhorst@tu-dresden.de>
2022-03-30 11:03:22 +0200

    Update download-single-transcript.py

--- youtube/download-single-transcript.py
+++ youtube/download-single-transcript.py
@@ -12,17 +12,20 @@ id = sys.argv[1]
 
 title = HTMLSession().get('https://www.youtube.com/watch?v=' + id).html.find('title', first=True).text
 
-result = {}
-transcript = YouTubeTranscriptApi.get_transcript(id, languages=['de'])
-text = ""
-lines = []
+videos_ids = [id]
 
-for line in transcript:
-	lines.append(line['text'])
+for id in video_ids:
+	result = {}
+	transcript = YouTubeTranscriptApi.get_transcript(id, languages=['de'])
+	text = ""
+	lines = []
 
-result['title'] = title.replace(" - YouTube", "")
-result['video_id'] = id
-result['transcript'] = lines
+	for line in transcript:
+		lines.append(line['text'])
 
-json_string = json.dumps(result, ensure_ascii=False, indent=4).encode('utf8')
-open(RESULTS_DIR + id + ".json", "w").write(json_string.decode())
+	result['title'] = title.replace(" - YouTube", "")
+	result['video_id'] = id
+	result['transcript'] = lines
+
+	json_string = json.dumps(result, ensure_ascii=False, indent=4).encode('utf8')
+	open(RESULTS_DIR + id + ".json", "w").write(json_string.decode())
